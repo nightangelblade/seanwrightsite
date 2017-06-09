866212bcc75c7e4114f8a539851cc0b3355af036
nightangelblade <sean.syc.wright@gmail.com>
2017-06-08 19:39:47 -0700

    Set up project model and migration.

---
+++ db/migrate/20170609023114_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[5.0]
+  def change
+    create_table :projects do |t|
+    	t.string :title
+    	t.date :start_date
+    	t.date :end_date
+    	t.string :technology
+    	t.string :description
+    	t.string :project_link
+      t.timestamps
+    end
+  end
+end
